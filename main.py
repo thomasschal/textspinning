ff24e5a20c7fd665744e4632dccf62539ac08661
unknown <schalthomas@gmail.com>
2021-05-15 20:05:25 +0200

    latest

--- main.py
+++ main.py
@@ -3,7 +3,30 @@ import string
 import nltk
 #nltk.download('punkt')
 from nltk.translate.bleu_score import sentence_bleu
+import math
+import re
+from collections import Counter
 
+WORD = re.compile(r"\w+")
+
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
 
 
 # Returns True if text is spun, returns False if text is equal
@@ -65,10 +88,11 @@ for line in corpusfile.readlines():
     text_list.append(line.split("\t")[1].rstrip()) # remove leading number, tab and trailing line feed
     #print(line.split("\t")[1])
 
-'''
-for sentence in text_list:
-    print_results(models[0],src_lang,tgt_lang,sentence)
-'''
 
 for sentence in text_list:
+    print_results(models[0],src_lang,tgt_lang,sentence)
+
+'''
+for sentence in text_list:
     print_result_tokens(models[0],src_lang,tgt_lang,sentence)
+    '''
