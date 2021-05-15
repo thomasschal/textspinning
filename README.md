f2e5493cda542750c3f85770c114bd813eee6d39
unknown <schalthomas@gmail.com>
2021-05-15 17:40:00 +0200

    Added news text corpus from Uni Leipzig and added functions for tokenization and comparing tokens

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This project researches the possiblity of spinning texts via translation and re-
 
 ## Resources
 
-* [dl-Translate](https://pypi.org/project/dl-translate/)
+* [dl-Translate](https://pypi.org/project/dl-translate/) - Translation package utilizing huggingface transformers
+* [nltk](https://pypi.org/project/nltk/) - Natural Language Toolkit, used to tokenize and calculate BERTScore
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import dl_translate as dlt
+import nltk
+from nltk.translate.bleu_score import sentence_bleu
+
 
 
 # Returns True if text is spun, returns False if text is equal
 def evaluate_output(input, output):
+    if input == output:
+        return False
+    else:
+        return True
+
+# Returns True if text is spun, returns False if text is equal
+def evaluate_output_tokens(input, output):
     if input == output:
         return False
     else:
@@ -24,11 +34,36 @@ def print_results(model, src_lang, tgt_lang, input_text):
     print("Spun:   " + str(evaluate_output(input_text, output)))
 
 
+# Prints input, output and evaluation line by line
+def print_result_tokens(model, src_lang, tgt_lang, input_text):
+    output = spin(model=model, src_lang=src_lang, tgt_lang=tgt_lang, input_text=input_text)
+    output_tokens = nltk.word_tokenize(output)
+    input_tokens = nltk.word_tokenize(input_text)
+    print("-------------------------------------------------------------------------------------------------------")
+    print("Input:  " + input_tokens)
+    print("Output: " + output_tokens)
+    print("Spun:   " + str(evaluate_output(input_tokens, output_tokens)))
+
+
 models = [dlt.TranslationModel("mbart50"), dlt.TranslationModel("m2m100")]
 model = dlt.TranslationModel("mbart50")
 src_lang = "German"
 tgt_lang = "English"
 input_text = "Der UN-Generalsekretär sprach auf der letzten Pressekonferenz von Angriffen auf die Persönlichkeitsrechte der Bürger Südamerikas."
 
-print_results(model=models[0], src_lang=src_lang,tgt_lang=tgt_lang,input_text=input_text)
+#print_results(model=models[0], src_lang=src_lang,tgt_lang=tgt_lang,input_text=input_text)
 
+
+text_list = []
+corpusfile = open('res\deu_de-news-wrt_2019_1K\deu_de-news-wrt_2019_1K-sentences.txt', 'r', encoding='utf-8')
+for line in corpusfile.readlines():
+    text_list.append(line.split("\t")[1].rstrip()) # remove leading number, tab and trailing line feed
+    #print(line.split("\t")[1])
+
+'''
+for sentence in text_list:
+    print_results(models[0],src_lang,tgt_lang,sentence)
+'''
+
+for sentence in text_list:
+    print_result_tokens(models[0],src_lang,tgt_lang,sentence)
