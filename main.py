578f88c08605e51d009f145f22920e6ed43335bc
unknown <schalthomas@gmail.com>
2021-05-16 23:55:32 +0200

    Added cosine similarity

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import dl_translate as dlt
 import string
 import nltk
+import numpy
 #nltk.download('punkt')
 from nltk.translate.bleu_score import sentence_bleu
 import math
 import re
 from collections import Counter
-
-WORD = re.compile(r"\w+")
-
-
-def get_cosine(vec1, vec2):
-    intersection = set(vec1.keys()) & set(vec2.keys())
-    numerator = sum([vec1[x] * vec2[x] for x in intersection])
-
-    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
-    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-    if not denominator:
-        return 0.0
-    else:
-        return float(numerator) / denominator
+from strsimpy.cosine import Cosine
 
 
-def text_to_vector(text):
-    words = WORD.findall(text)
-    return Counter(words)
+def cosine(input_text, output_text):
+    cosine = Cosine(2)
+    p0 = cosine.get_profile(input_text)
+    p1 = cosine.get_profile(output_text)
+    return cosine.similarity_profiles(p0, p1)
+
+
+def text_to_vector(tokens):
+    return Counter(tokens)
+
+
+def cosine_distance(u, v):
+    """
+    Returns the cosine of the angle between vectors v and u. This is equal to
+    u.v / |u||v|.
+    """
+    return numpy.dot(u, v) / (math.sqrt(numpy.dot(u, u)) * math.sqrt(numpy.dot(v, v)))
 
 
 # Returns True if text is spun, returns False if text is equal
@@ -71,6 +71,7 @@ def print_result_tokens(model, src_lang, tgt_lang, input_text):
     print("Input:  " + ' '.join(input_tokens))
     print("Output: " + ' '.join(output_tokens))
     print("Spun:   " + str(evaluate_output(input_tokens, output_tokens)))
+    print("Cosine: " + str(cosine(input_text, output)))
 
 
 models = [dlt.TranslationModel("mbart50"), dlt.TranslationModel("m2m100")]
@@ -88,11 +89,10 @@ for line in corpusfile.readlines():
     text_list.append(line.split("\t")[1].rstrip()) # remove leading number, tab and trailing line feed
     #print(line.split("\t")[1])
 
-
-for sentence in text_list:
-    print_results(models[0],src_lang,tgt_lang,sentence)
-
 '''
 for sentence in text_list:
+    print_results(models[0],src_lang,tgt_lang,sentence)
+'''
+
+for sentence in text_list:
     print_result_tokens(models[0],src_lang,tgt_lang,sentence)
-    '''
